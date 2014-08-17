99d0de8c6f67e5c44dd4943d3992d88b38c9efca
SnakeWorker <774350802@qq.com>
2014-08-17 17:52:23 +0800

    refactor code second

--- js/data_cart.js
+++ js/data_cart.js
@@ -4,52 +4,21 @@
 $(document).ready(function(){
     initCartNumber();
     initCart();
-    $('.increase').on('click',function(event){
-        event.preventDefault();
-        plus($(this));
-    });
-    $('.decrease').on('click',function(event){
-        event.preventDefault();
-        minus($(this));
-    });
-    $('.delete').on('click',function(event){
-        event.preventDefault();
-        deleteItem($(this));
+
+    $('.delete').on('click',function(){
+        changeCount($(this).data('name'),0);
+        $(this).closest('.item').remove();
     });
     $('.number').on('change',function(){
-        updateItem($(this));
+        var number = $(this).val();
+        if(number == 0){
+            $(this).closest('.item').remove();
+        }
+        changeCount($(this).data('name'),parseInt(number));
     });
 });
 
-function updateItem(element){
-    var number = element.val();
-    console.log(number);
-    var name = element.data('name');
-    changeCount(element,name,parseInt(number));
-}
-
-function deleteItem(element){
-    var input = element.parent().find('.number');
-    var name = element.data('name');
-    changeCount(input,name,0);
-    element.closest('.item').remove();
-}
-
-function minus(element){
-    var input = element.parent().find('.number');
-    input.val(parseInt(input.val())-1);
-    var name = element.data('name');
-    changeCount(input,name,parseInt(input.val()));
-}
-
-function plus(element){
-    var input = element.parent().find('.number');
-    input.val(parseInt(input.val())+1);
-    var name = element.data('name');
-    changeCount(input,name,parseInt(input.val()));
-}
-
-function changeCount(element,name,number){
+function changeCount(name,number){
 
     var cart = Util.storage.getStorageItem('cart');
     _.forEach(cart.cartItems,function(item){
@@ -61,7 +30,7 @@ function changeCount(element,name,number){
         }
     });
     cart = new Cart(cart);
-    element.val(number);
+    //element.val(number);
     $('#buy').text('Total : $' +cart.getTotalMoney()+", And Pay it Now >>>");
     $('#cart').text('Cart(' + cart.getCount() + ')');
     Util.storage.add2Storage('cart',cart);
@@ -76,12 +45,8 @@ function initCart(){
         }
         var text = "<div class='row text-center item'><div class='col-md-2'>"+item.getProductName()+
             "</div><div class='col-md-4'><div class='form-inline form-group'>"+
-            "<button class='btn btn-warning decrease' data-name="+item.getProductName()+
-            "><span class='glyphicon glyphicon-minus'></span></button>"+
             "<input type='number' class='form-control number' name='number' data-name='"+item.getProductName()+"' value='"+item.getCount()+"'>" +
-            "<button data-name="+item.getProductName()+" class='btn btn-success increase'>"+
-            "<span class='glyphicon glyphicon-plus'></span></button></div></div><div class='col-md-2'>$"+
-            item.getPrice().toFixed(2)+"</div>"+
+            "</div></div><div class='col-md-2'>$"+item.getPrice().toFixed(2)+"</div>"+
             "<div class='col-md-2'>$<span id='"+item.getProductName()+"'>"+item.getSubtotal().toFixed(2)+"</span></div><div class='col-md-2'><a href='#'>"+
             "<span class='glyphicon glyphicon-remove text-danger delete' data-name='"+item.getProductName()+"'></span></a></div></div>";
         $('#cart_panel').append(text);
